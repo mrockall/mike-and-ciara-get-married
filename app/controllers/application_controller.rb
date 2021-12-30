071b0b586e4d5360d9bffb10ded67f83435376b0
Mike Rockall <mike@exordo.com>
2021-12-30 18:50:12 +0000

    Added a random question render and answer selection.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   def index
-    # no-op for now
+    @question = Question.order(Arel.sql('RANDOM()')).first
+  end
+
+  def answer
+    @answer = Answer.find(params[:answer_id])
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/manage', as: 'rails_admin'
 
+  post 'answer', to: 'application#answer'
+
   root 'application#index'
 end
