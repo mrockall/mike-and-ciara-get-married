373404a43778b47a789eaaa53e660e11a874c4a4
Mike Rockall <mike@exordo.com>
2022-06-13 17:32:45 +0100

    Added photos to the photo grid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # Lazy load images by default
+  config.action_view.image_loading = "lazy"
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
