855fcfb0e2fbf78151496413b275f6f4a5f3a5c4
Mike Rockall <mike@exordo.com>
2022-06-13 17:55:37 +0100

    Still trying to get the assets to compile even though I don't know why I need scss.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  # Turn this off as I don't need anything further from Rails on CSS. Tailwind does all that.
+  config.assets.css_compressor = nil
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
